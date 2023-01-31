bf8bbca984e4503d867d2f8579dddac037c0b410
faath <53069342+faath@users.noreply.github.com>
2023-01-31 17:57:04 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ function console_menu() {
     echo '[01] - Instalar CheckUser'
     echo '[02] - Atualizar CheckUser'
     echo '[03] - Desinstalar CheckUser'
-    echo '[00] - Sair (by: @Kaua_VPN)'
+    echo '[00] - Sair (by: @PoderosoChefao)'
 
     read -p 'Escolha uma opção: ' option
 
